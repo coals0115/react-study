f66eb75211f8cbee84b99860bb1e6a7e68e8888f
coals0115 <slaoc0115@gmail.com>
2023-09-20 10:14:16 +0900

    8장 Hooks) 8.3 useReducer

--- src/08/Counter.js
+++ src/08/Counter.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 
-const Counter = () => { // Counter 컴포넌트 생성
+const CounterBak = () => { // Counter 컴포넌트 생성
     // iv와 변경할 함수 하나를 받음
     const [value, setValue] = useState(0); // 0은 초기값
 
@@ -14,5 +14,32 @@ const Counter = () => { // Counter 컴포넌트 생성
         </div>
       );
 };
+
+function reducer(state, action) {
+    // action.type에 따라 다른 작업 수행
+    switch(action.type) {
+        case 'INCREMENT':
+            return { value: state.value + 1 };
+        case 'DECREMENT':
+            return { value: state.value -1 };
+        default:
+            // 아무것도 해당되지 않을 때 기존 상태 반환
+            return state;
+    }
+}
+
+const Counter = () => {
+    const [state, dispatch] = useReducer(reducer, { value: 0 });
+  
+    return (
+      <div>
+        <p>
+          현재 카운터 값은 <b>{state.value}</b> 입니다.
+        </p>
+        <button onClick={() => dispatch({ type: 'INCREMENT' })}>+1</button>
+        <button onClick={() => dispatch({ type: 'DECREMENT' })}>-1</button>
+      </div>
+    );
+  };
 
 export default Counter;
