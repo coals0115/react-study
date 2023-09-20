37d641f898006ad94dfdaa73978dd9ec502fc825
coals0115 <slaoc0115@gmail.com>
2023-09-20 10:25:57 +0900

    8장 Hooks) 8.5 useCallback

--- src/08/Average.js
+++ src/08/Average.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 
 const getAverage = numbers => {
   console.log('평균값 계산중..');
@@ -11,9 +11,19 @@ const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState('');
 
-  const onChange = e => { // 리렌더링 할 때마다 람다식이 새로 만들어진다. 이런 비효율을 없애려고 만든 게 useCallback
+//   const onChange = e => { // 리렌더링 할 때마다 람다식이 새로 만들어진다. 이런 비효율을 없애려고 만든 게 useCallback
+//     setNumber(e.target.value);
+//   };
+
+  // useCallback = 성능 최적화 시 사용
+  /**
+   * 플라이웨이트 패턴(ex. 서블릿, 스프링)
+   * -> 맵으로 관리, 재사용
+   * 계속 새로 생성하지 않고 한번 만들어둔 거 따로 저장해두고있다가 재사용한다.
+   */
+  const onChange = useCallback(e => {
     setNumber(e.target.value);
-  };
+  }, []); // 컴포넌트가 처음 렌더링될 때만 함수 생성
 
   const onInsert = () => {
     const nextList = list.concat(parseInt(number));
